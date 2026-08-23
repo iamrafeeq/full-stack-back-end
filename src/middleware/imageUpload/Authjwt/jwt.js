e8d0459d46a35ba672e44c718b12f5cb6e9235e0
iamrafeeq <rafeeqahmed209087@gmail.com>
2026-08-23 15:50:29 +0500

    fix: extend JWT expiry to 7d, add 401 interceptor to auto-logout

--- src/middleware/imageUpload/Authjwt/jwt.js
+++ src/middleware/imageUpload/Authjwt/jwt.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 const getTokenFromHeader = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
 export default getTokenFromHeader;
